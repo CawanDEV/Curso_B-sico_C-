663af4c89ff8d1c173f9a3ed0ecf8163e6a12eea
Cawan Davi <cawandev@gmail.com>
2024-09-04 16:22:30 -0300

    feat: OBJECT ORIENTED

---
+++ CursoBasicoCSHARP/CursoBasicoCSHARP/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBasicoCSHARP
+{
+    public class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+        public char Genero;
+        private bool Aprovado;
+         
+        public void MostrarDados()
+        {
+            Console.WriteLine($"Nome : {Nome}");
+            Console.WriteLine($"Idade : {Idade} anos");
+            Console.WriteLine($"Genero : {Genero}");
+            Console.WriteLine($"Aprovado : {Aprovado}");
+        }
+    }
+}
