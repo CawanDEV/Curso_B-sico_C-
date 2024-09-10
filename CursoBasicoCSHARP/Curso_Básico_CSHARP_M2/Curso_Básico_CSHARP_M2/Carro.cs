2a3fdd3ae355a438512d53fc9022be7308a265ed
Cawan Davi <cawandev@gmail.com>
2024-09-10 14:52:01 -0300

    feat: builders and destroyers

---
+++ CursoBasicoCSHARP/Curso_Básico_CSHARP_M2/Curso_Básico_CSHARP_M2/Carro.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_Básico_CSHARP_M2
+{
+    internal class Carro
+    {
+        public int Portas {  get; set; }
+
+        public double Preco { get; set; }
+
+        public string Modelo { get; set; }
+
+        public Carro() 
+        {
+            Console.WriteLine("Um Novo Objeto Carro Foi Criado.");
+        }
+
+        public Carro(string modelo) : this() 
+        {
+            this.Modelo = modelo;
+        }
+
+        public Carro(string modelo, int portas, double preco) : this(modelo)
+        {
+            this.Portas = portas;
+            this.Preco = preco;
+        }
+
+
+    }
+}
--- CursoBasicoCSHARP/Curso_Básico_CSHARP_M2/Curso_Básico_CSHARP_M2/Program.cs
+++ CursoBasicoCSHARP/Curso_Básico_CSHARP_M2/Curso_Básico_CSHARP_M2/Program.cs
@@ -6,7 +6,22 @@ namespace Curso_Básico_CSHARP_M2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Carro c1 = new Carro();
+            c1.Modelo = "Fusca";
+            c1.Portas = 4;
+            c1.Preco = 29990;
+
+            Carro c2 = new Carro();
+            c2.Modelo = "Opala";
+            c2.Portas = 4;
+            c2.Preco = 49990;
+
+            Carro c3 = new Carro("Chevete");
+            c3.Portas = 2;
+            c3.Preco = 32990;
+
+            Carro c4 = new Carro("Monza", 4, 43990);
+                  
         }
     }
 }
