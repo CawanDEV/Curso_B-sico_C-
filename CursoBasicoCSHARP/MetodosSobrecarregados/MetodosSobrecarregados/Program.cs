eeefdd74d3d342d59582ba48b43cad53f380239d
Cawan Davi <cawandev@gmail.com>
2024-09-10 16:56:55 -0300

    feat: Overloaded Methods

--- CursoBasicoCSHARP/MetodosSobrecarregados/MetodosSobrecarregados/Program.cs
+++ CursoBasicoCSHARP/MetodosSobrecarregados/MetodosSobrecarregados/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace MetodosSobrecarregados
 {
@@ -6,7 +7,11 @@ namespace MetodosSobrecarregados
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Veiculo v1 = new Veiculo("Fusca", CorVeiculo.Branco, 2);
+            v1.MostrarDados(1);
+            Veiculo v2 = new Veiculo("Opala", CorVeiculo.Preto, 4);
+            v2.MostrarDados(2);
+            
         }
     }
 }
---
+++ CursoBasicoCSHARP/MetodosSobrecarregados/MetodosSobrecarregados/Veiculo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MetodosSobrecarregados
+{
+    enum CorVeiculo
+    {
+        Branco,
+        Preto,
+        Vermelho,
+        Prata,
+        Grafite
+    }
+    internal class Veiculo
+    {
+        public string Modelo { get; set; }
+        public int Peso { get; set; }
+        public double Velocidade { get; set; }
+        public int Portas { get; set; }
+        public CorVeiculo Cor { get; set; }
+
+        public Veiculo(string modelo)
+        {
+            this.Modelo = modelo;
+        }
+        public Veiculo(string modelo, CorVeiculo cor) : this(modelo)
+        {
+            this.Cor = cor;
+        }
+        public Veiculo(string modelo, CorVeiculo cor, int portas = 4) : this(modelo, cor)
+        {
+            this.Portas = portas;
+        }
+        public void MostrarDados()
+        {
+            Console.WriteLine($"Veiculo {this.Modelo} :: Cor {this.Cor} :: {this.Portas} Portas");
+        }
+        public void MostrarDados(int nroLinha)
+        {
+            Console.WriteLine($"{nroLinha}. Veiculo {this.Modelo} :: Cor {this.Cor} :: {this.Portas} Portas");
+        }
+        public void Acelerar()
+        {
+            this.Velocidade += 10;
+        }
+        public void Acelerar (int acrescimoPorSegundo, double tempoS)
+        {
+            DateTime inicio = DateTime.Now;
+            DateTime fim = inicio.AddSeconds(tempoS);
+            while (inicio < fim)
+            {
+                this.Velocidade += acrescimoPorSegundo;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
