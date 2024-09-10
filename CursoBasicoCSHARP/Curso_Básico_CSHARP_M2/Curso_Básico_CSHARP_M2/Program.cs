e1930dc3e0703c2f27587e9fd906c418553af9c5
Cawan Davi <cawandev@gmail.com>
2024-09-10 14:31:30 -0300

    feat: builders and destroyers

---
+++ CursoBasicoCSHARP/Curso_Básico_CSHARP_M2/Curso_Básico_CSHARP_M2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curso_Básico_CSHARP_M2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
