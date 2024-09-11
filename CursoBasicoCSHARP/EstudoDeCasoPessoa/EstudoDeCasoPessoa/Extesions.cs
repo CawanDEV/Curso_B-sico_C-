11977c2b5ddc781fcc235fe4419af8316b5b9bd7
Cawan Davi <cawandev@gmail.com>
2024-09-11 16:53:20 -0300

    feat: Triggered limit and character type in cpf

---
+++ CursoBasicoCSHARP/EstudoDeCasoPessoa/EstudoDeCasoPessoa/Extesions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoDeCasoPessoa
+{
+    static class Extesions
+    {
+        public static bool HasOnlyDigits(this string valor)
+        {
+            const string digitos = "0123456789";
+            foreach (char c in digitos)
+            {
+                if(!digitos.Contains(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ CursoBasicoCSHARP/EstudoDeCasoPessoa/EstudoDeCasoPessoa/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EstudoDeCasoPessoa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa("Albert", "Einstein", new DateTime(1955, 6, 17), "98765432345");
+            p1.Peso = 76;
+            p1.Altura = 1.76;
+            p1.MostrarDados();
+            p1.Comer(2.5);
+            p1.Comer(3800);
+            p1.Correr(7, 30);
+            p1.MostrarDados();
+
+            Console.WriteLine();
+
+            Pessoa p2 = new Pessoa("Ada", "LoveLace", new DateTime(1975, 4, 18), "12345678910", 62, 1.65);
+            p2.MostrarDados();
+        }
+    }
+}
