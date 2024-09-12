db25fb8b5370636b435d644fba163d7f04cdfd57
Cawan Davi <cawandev@gmail.com>
2024-09-12 02:47:38 -0300

    feat: Calculadora IMC

---
+++ CursoBasicoCSHARP/CalculadoraIMC/CalculadoraIMC/Calculadora.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraIMC
+{
+    internal class Calculadora
+    {
+        public double Altura { get; set; }
+        public double Peso { get; set; }
+        public double IMC
+        {
+            get 
+            {
+                return (Peso / (Altura * Altura));
+            }
+        }
+
+        public void exibirInformacoes()
+        {
+            Console.WriteLine($"Seu IMC é de: {IMC:f2}");
+        }
+    }
+}
---
+++ CursoBasicoCSHARP/CalculadoraIMC/CalculadoraIMC/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CalculadoraIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+            Console.WriteLine("Bem Vindo a Calculadora de IMC");
+            Console.WriteLine("Escreva Seu Peso:");
+            calc.Peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escreva Sua Altura: ");
+            calc.Altura = double.Parse(Console.ReadLine());
+
+            calc.exibirInformacoes();
+        }
+    }
+}
