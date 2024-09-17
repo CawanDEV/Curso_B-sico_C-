11c9a7987583a2fd275dd6715c087ba565951708
Cawan Davi <cawandev@gmail.com>
2024-09-17 16:16:18 -0300

    feat: new program

--- CursoBasicoCSHARP/Polimorfismo/Polimorfismo/Program.cs
+++ CursoBasicoCSHARP/Polimorfismo/Polimorfismo/Program.cs
@@ -9,7 +9,20 @@ namespace Polimorfismo
         static Random random = new Random();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Não é possivel instanciar a classe Veiculo, pois é abstrata
+            //Veiculo v1 = new Veiculo(1000, new DateTime(25, 10, 2000))
+            Veiculo v2 = new Carro(1100, DateTime.Now.Date.AddDays(-600));
+            Veiculo v3 = new Onibus(6000, DateTime.Now.Date.AddDays(-5800));
+
+            v2.Abastecer(40);
+            v3.Abastecer(120);
+
+            v2.Mover(30);
+            v3.Mover(100);
+
+            v2.Frear();
+            v3.Frear();
+
         }
     }
 }
